59578472b0708cdd862b1258eca13916ad914878
Shubham Meshram <meshram.shubham27@gmail.com>
2022-04-05 23:51:14 +0530

    updated the homepage template

--- main.js
+++
@@ -1,8 +0,0 @@
-const sapceButton = document.querySelector("#space");
-const display = document.querySelector(".display");
-function displayText(result) {
-    keyType.keyTypeArea.value = keyType.keyTypeArea.value + result;
-    sapceButton.onclick = () => {
-        keyType.keyTypeArea.value += " ";
-    };
-}
